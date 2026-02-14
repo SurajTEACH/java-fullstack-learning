08098ea7ab3fa61a3908a933f487ff3f86cd6ee8
email@female.com <surajshah1509>
2026-02-14 08:58:17 +0530

    Added ExampleMarks.java

---
+++ core_java/data_type/ExampleMarks.java
@@ -0,0 +1,15 @@
+package core_java.data_type;
+
+public class ExampleMarks {
+    public  static void main(String[] args){
+        String name = "Sam";
+
+        int rollNumber = 1;
+
+        double percentageMarks = 99.95;
+
+        char  result =  'P';
+
+        System.out.println("Display the marks of the student : \n"+"name : " +name + "\n roll  number : " + rollNumber + "\n percentage marks : " + percentageMarks + "\n result : " + result);
+    }
+}
