6275b88348e870e2dea47d429a4bd20221944907
email@female.com <surajshah1509>
2026-02-15 23:40:01 +0530

    added core_java/data_type/PracticeLevel2AreaofTrangle.java

---
+++ core_java/data_type/PracticeLevel2AreaofTrangle.java
@@ -0,0 +1,18 @@
+package core_java.data_type;
+
+import java.util.Scanner;
+
+public class PracticeLevel2AreaofTrangle {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the base of the triangle:");
+        double base = sc.nextDouble();
+        System.out.println("Enter the height of the triangle:");
+        double height = sc.nextDouble();
+        double area = (base * height) / 2;
+        double in =  2.54;
+        double cm = area * in;
+
+        System.out.println("The area of the triangle is: " + area + " square inches and " + cm + " square centimeters.");
+    }
+}
