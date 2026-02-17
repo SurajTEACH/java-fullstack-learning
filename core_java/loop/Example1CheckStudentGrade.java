bdb0000f0498f4353664bdc311d760ff91efa03f
email@female.com <surajshah1509>
2026-02-17 12:52:28 +0530

    added Example1CheckStudentGrade.java

---
+++ core_java/loop/Example1CheckStudentGrade.java
@@ -0,0 +1,20 @@
+package core_java.loop;
+
+import java.util.Scanner;
+
+public class Example1CheckStudentGrade {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the student's grade ");
+        int studentGrade = sc.nextInt();
+
+        if(studentGrade >=50){
+            System.out.println("Congratulations!");
+            System.out.println("You have passed the exam");
+        }
+        else{
+              System.out.println("Sorry!");
+              System.out.println("You have failed the exam");
+            }
+    }
+}
