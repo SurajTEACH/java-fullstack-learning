ccf815766b0829071b115aa1fd397559fdf42538
email@female.com <surajshah1509>
2026-02-19 16:27:34 +0530

    commit  level1

---
+++ core_java/loop_ifelse/PrcaticsLevel1SumOfN.java
@@ -0,0 +1,9 @@
+package core_java.loop_ifelse;
+
+public class PrcaticsLevel1SumOfN {
+    public static void main(String[] args){
+         int n = 5;
+        int sum = n  * (n+1) / 2;
+        System.out.println("The sum of first " + n + " natural numbers is: " + sum);
+    }
+}
