288bb58158ad716195d3d26bcac450b853708c8b
email@female.com <surajshah1509>
2026-02-20 15:25:01 +0530

    added file PrcaticsLevel2FizzBuzz.java

---
+++ core_java/loop_ifelse/PrcaticsLevel2FizzBuzz.java
@@ -0,0 +1,28 @@
+package core_java.loop_ifelse;
+
+import java.util.Scanner;
+
+public class PrcaticsLevel2FizzBuzz {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int num =  sc.nextInt();
+
+        if(num > 0){
+             for(int i = 1; i<=num;i++){
+                  if( i%3 == 0 && i%5 == 0){
+                      System.out.println("FizzBuzz");
+                  } else if(i % 3 == 0){
+                      System.out.println("Fizz");
+                  } else if(i % 5 == 0){
+                      System.out.println("Buzz");
+                  } else {
+                      System.out.println(i);
+                  }
+             }
+        }
+    }
+}
+
+
+/*Write a program FizzBuzz, take a number as user input, and check for a positive integer. If positive integer, loop and print the number, but for multiples of 3 print "Fizz" instead of the number, for multiples of 5 print "Buzz", and for multiples of both print "FizzBuzz".
+Hint => a. Take the user input number, check for a positive integer, and use for loop to display */
